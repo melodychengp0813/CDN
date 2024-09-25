2a0384a772317ab754a08c7d50a427a80c22749c
melodycheng <w629329@gmail.com>
2024-09-25 11:06:40 +0800

    git init

---
+++ vite-by-melody/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from 'vue'
+    const num = ref(2);
+           function clickMe(){
+            num.value++
+           }
+            //console.log(num.value);
+            //setInterval(() =>{
+              //  num.value++
+            //},1000);
+
+           
+</script>
+
+<template>
+  <div id="app">
+     {{ num }}
+     <button type="button" @click="clickMe">ClickMe!!!</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
